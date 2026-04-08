ced90b1c4b52a8a7217ef7f73072775ff9320712
pandacoelho5-gif <pandacoelho5@gmail.com>
2026-04-08 14:51:35 -0300

    Create App.js structure for the mobile app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import MatchDetailsScreen from './MatchDetailsScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Match Details" component={MatchDetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
